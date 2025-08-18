7da1322ea400c97404f3915ba8e34e827323b243
Hamed <hamedaravane@gmail.com>
2025-08-19 00:17:29 +0330

    refactor(layout): simplify profile page main container styles

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { Spinner } from '@heroui/react';
 import React from 'react';
 
 import AppLayout from '@/libs/common/components/AppLayout';
@@ -7,7 +8,6 @@ import SummaryOrdersSection from '@/libs/orders/components/summary-orders-sectio
 import PreviewStoresSection from '@/libs/stores/components/preview-stores-section';
 import ProfileCard from '@/libs/users/components/profile-card';
 import { useUserState } from '@/libs/users/context/userContext';
-import { Spinner } from '@heroui/react';
 
 export default function ProfilePage() {
   const { data, isLoading } = useUserState();
@@ -24,7 +24,7 @@ export default function ProfilePage() {
 
   return (
     <AppLayout>
-      <main className="mx-auto max-w-2xl space-y-10 px-4 py-6">
+      <main className="mx-auto space-y-10 py-6">
         <ProfileCard user={data} />
         {data.stores && <PreviewStoresSection stores={data.stores} title="My Stores" />}
         {data.orders && <SummaryOrdersSection orders={data.orders} title="Recent Orders" />}
