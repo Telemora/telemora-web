0224e72cdf10d832d78c5fb2c281a687375583ba
Hamed <hamedaravane@gmail.com>
2025-08-23 17:00:32 +0330

    refactor(components): abstract `CarouselItem` and simplify `Carousel` implementation
    - Introduce `CarouselItem` sub-component to reduce code duplication
    - Use dynamic data (`banners` array) for rendering carousel slides
    - Update banner files for consistency in design

--- src/libs/common/components/Carousel.tsx
+++ src/libs/common/components/Carousel.tsx
@@ -1,8 +1,14 @@
 import useEmblaCarousel from 'embla-carousel-react';
 import Autoplay from 'embla-carousel-autoplay';
+import { cn } from '@heroui/react';
 
-{
-  /* eslint-disable @next/next/no-img-element */
+/* eslint-disable @next/next/no-img-element */
+interface Props {
+  title: string;
+  subtitle: string;
+  imageSrc: string;
+  imageAlt: string;
+  position: 'end' | 'start';
 }
 
 export function Carousel() {
@@ -20,61 +26,73 @@ export function Carousel() {
       }),
     ],
   );
+  const banners: Props[] = [
+    {
+      title: 'Global trading',
+      subtitle: 'with crypto-native simplicity',
+      imageSrc: '/banners/banner-1.png',
+      imageAlt: 'global',
+      position: 'start',
+    },
+    {
+      title: 'Telegram login',
+      subtitle: 'no extra signups needed',
+      imageSrc: '/banners/banner-2.png',
+      imageAlt: 'login',
+      position: 'end',
+    },
+    {
+      title: 'Sell & Buy anything',
+      subtitle: 'goods, services, or digital content',
+      imageSrc: '/banners/banner-3.png',
+      imageAlt: 'trade',
+      position: 'start',
+    },
+  ];
 
   return (
     <div className="overflow-hidden" ref={emblaRef}>
       <div className="flex">
-        <div className="min-w-0 px-2" style={{ flex: '0 0 100%' }}>
-          <div className="relative">
-            <span className="absolute start-3 top-1/3 font-semibold text-white drop-shadow-sm">
-              Global trading
-            </span>
-            <p className="absolute start-3 top-1/2 max-w-1/2 text-xs text-white drop-shadow-lg">
-              with crypto-native simplicity
-            </p>
-            <img
-              src="/banners/banner-1.png"
-              className="overflow-clip rounded-lg"
-              alt="banner-1"
-              width={768}
-              height={384}
-            />
-          </div>
-        </div>
-        <div className="min-w-0 px-2" style={{ flex: '0 0 100%' }}>
-          <div className="relative">
-            <span className="absolute start-2/5 top-1/3 font-semibold text-white drop-shadow-sm">
-              Telegram login
-            </span>
-            <p className="absolute start-2/5 top-1/2 max-w-1/2 text-xs text-white drop-shadow-lg">
-              no extra signups needed
-            </p>
-            <img
-              src="/banners/banner-2.png"
-              className="overflow-clip rounded-lg"
-              alt="banner-1"
-              width={768}
-              height={384}
-            />
-          </div>
-        </div>
-        <div className="min-w-0 px-2" style={{ flex: '0 0 100%' }}>
-          <div className="relative">
-            <span className="absolute start-3 top-1/3 font-semibold text-white drop-shadow-sm">
-              Sell anything
-            </span>
-            <p className="absolute start-3 top-1/2 max-w-1/2 text-xs text-white drop-shadow-lg">
-              goods, services, or digital content
-            </p>
-            <img
-              src="/banners/banner-3.png"
-              className="overflow-clip rounded-lg"
-              alt="banner-1"
-              width={768}
-              height={384}
-            />
-          </div>
-        </div>
+        {banners.map((item, i) => {
+          return (
+            <div className="min-w-0 px-2" style={{ flex: '0 0 100%' }} key={i}>
+              <CarouselItem {...item} />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export function CarouselItem(item: Props) {
+  const position = item.position === 'start' ? 'start-3' : 'start-1/2';
+  return (
+    <div className="min-w-0 px-2" style={{ flex: '0 0 100%' }}>
+      <div className="relative">
+        <span
+          className={cn(
+            'absolute top-1/3 max-w-1/2 text-xs font-semibold text-white drop-shadow-lg',
+            position,
+          )}
+        >
+          {item.title}
+        </span>
+        <p
+          className={cn(
+            'absolute top-1/2 max-w-1/2 text-xs font-semibold text-white drop-shadow-lg',
+            position,
+          )}
+        >
+          {item.subtitle}
+        </p>
+        <img
+          src={item.imageSrc}
+          className="overflow-clip rounded-lg"
+          alt={item.imageAlt}
+          width={768}
+          height={384}
+        />
       </div>
     </div>
   );
