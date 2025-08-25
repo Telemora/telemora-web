df07163476c3555e169b54e5fffd3e7fd7412692
Hamed Arghavan <hamedaravane@gmail.com>
2025-08-25 14:11:29 +0330

    chore(theme): add color constants for light and dark themes
    - Introduce `lightThemeColors` and `darkThemeColors` in `colors.ts` for consistent theme styling.

---
+++ src/libs/common/constants/colors.ts
@@ -0,0 +1,22 @@
+export const lightThemeColors = {
+  antiflashWhite: '#f1f1f1ff',
+  battleshipGray: '#999999ff',
+  black: '#000000ff',
+  blueNcs: '#168acdff',
+  indianRed: '#d14e4eff',
+  pictonBlue: '#40a7e3ff',
+  platinum: '#e7e7e7ff',
+} as const;
+
+export const darkThemeColors = {
+  argentinianBlue: '#6ab2f2ff',
+  argentinianBlue2: '#6ab3f3ff',
+  gunmetal: '#232e3cff',
+  imperialRed: '#ec3942ff',
+  richBlack: '#17212bff',
+  richBlack2: '#111921ff',
+  silverLakeBlue: '#5288c1ff',
+  slateGray: '#708499ff',
+  white: '#ffffffff',
+  whiteSmoke: '#f5f5f5ff',
+} as const;
