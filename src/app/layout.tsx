baee362df8bf01940f48dcb88b4575c639cf276c
Hamed <hamedaravane@gmail.com>
2025-09-01 00:57:21 +0330

    chore(layout): update viewport width to use 'device-width' instead of 'device-fit'

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
 };
 
 export const viewport: Viewport = {
-  width: 'device-fit',
+  width: 'device-width',
   initialScale: 1,
   minimumScale: 1,
   maximumScale: 1,
