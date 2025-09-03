e81b8d351c323391565a3807a803ba4f24d4a83f
Hamed <hamedaravane@gmail.com>
2025-09-03 19:28:58 +0330

    style(BottomTabs): refine styles for consistency and responsiveness
    - Add conditional background color for large screens
    - Center tab list and adjust layout
    - Remove unused padding properties

--- src/libs/common/components/BottomTabs.tsx
+++ src/libs/common/components/BottomTabs.tsx
@@ -41,15 +41,12 @@ export default function BottomTabs() {
       items={tabList}
       color="primary"
       classNames={{
-        base: 'fixed bottom-0 z-50 w-full md:max-w-sm translate-x-1/2 right-1/2',
+        base: 'fixed bottom-0 z-50 bg-content1 lg:bg-transparent lg:max-w-sm w-full translate-x-1/2 right-1/2',
+        tabList: 'max-w-sm mx-auto',
         tab: 'h-16',
       }}
       style={{
         paddingBottom: 'var(--tg-safe-area-inset-bottom)',
-        paddingRight:
-          'calc(var(--tg-safe-area-inset-right) + var(--tg-content-safe-area-inset-right))',
-        paddingLeft:
-          'calc(var(--tg-safe-area-inset-left) + var(--tg-content-safe-area-inset-left))',
       }}
       onSelectionChange={(key) => route.push(key as string)}
     >
